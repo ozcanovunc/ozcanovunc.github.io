c47998230e197225db5825dfea9c6a55e762d0df
ozcanovunc <ozcanovunc@outlook.com>
2021-01-10 18:42:20 +0300

    Deploy Next.js to gh-pages

---
+++ _next/static/2QMW02Q0lNsx4w--c4KV2/_buildManifest.js
@@ -0,0 +1,12 @@
+self.__BUILD_MANIFEST = {
+  __rewrites: [],
+  "/": ["static\u002Fchunks\u002Fpages\u002Findex-db490a27e3f5414969bf.js"],
+  "/_error": [
+    "static\u002Fchunks\u002Fpages\u002F_error-6c87ae2bd84f598ce8c0.js",
+  ],
+  "/about": [
+    "static\u002Fchunks\u002Fpages\u002Fabout-7e803e5a7a5025a059bb.js",
+  ],
+  sortedPages: ["\u002F", "\u002F_app", "\u002F_error", "\u002Fabout"],
+};
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
